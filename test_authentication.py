31fbe7d79b11b324076a76e0da1a9098c0a6a7b3
Artem Izbyshev <izbyshev-aa@mail.ru>
2025-05-16 14:20:03 +0300

    hw_1_qa_guru_done_new_с поиском в яндексе

--- test_authentication.py
+++ test_authentication.py
@@ -1,5 +1,5 @@
-from selene import browser, have
-
+from selene import browser, be, have
+from selene.core.wait import Command
 
 def test_valid_login():
     browser.open('https://school.qa.guru/cms/system/login')
@@ -17,16 +17,22 @@ def test_wrong_password():
 
     browser.quit()
 
-    def test_empty_password():
-        browser.open('https://school.qa.guru/cms/system/login')
-        browser.element('[name=email]').type('qagurubot@gmail.com').press_enter()
-        browser.element('.btn-error').should(have.text('Не заполнено поле Пароль'))
+def test_empty_password():
+    browser.open('https://school.qa.guru/cms/system/login')
+    browser.element('[name=email]').type('qagurubot@gmail.com').press_enter()
+    browser.element('.btn-error').should(have.text('Не заполнено поле Пароль'))
 
-        browser.quit()
+    browser.quit()
 
-    def test_empty_login():
-        browser.open('https://school.qa.guru/cms/system/login')
-        browser.element('[name=password]').type('assdaff').press_enter()
-        browser.element('.btn-error').should(have.text('Не заполнено поле E-Mail'))
+def test_empty_login():
+    browser.open('https://school.qa.guru/cms/system/login')
+    browser.element('[name=password]').type('assdaff').press_enter()
+    browser.element('.btn-error').should(have.text('Не заполнено поле E-Mail'))
 
-        browser.quit()
+    browser.quit()
+
+def test_empty_login():
+    browser.open('https://ya.ru ')
+    browser.element('[name=text]').type('site:example.com "1234567890abcdefghijklmnopqrstuvwxyz" mime:pdf lang:zu date:19000101').press_enter()
+    browser.element('.EmptySearchResults-Title').should(have.text('Ничего не нашли'))
+    browser.quit()
